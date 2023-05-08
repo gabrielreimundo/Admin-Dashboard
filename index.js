f6cfbe6f1396a3c798b3fb961dd13b83e2e25835
Gabriel Reimundo <109987851+gabrielreimundo@users.noreply.github.com>
2023-05-08 01:11:45 -0300

    at

---
+++ index.js
@@ -0,0 +1,11 @@
+const sideMenu = document.querySelector("aside");
+const menuBtn = document.querySelector("#menu-btn");
+const closeBtn = document.querySelector("#close-btn");
+const themeToggler = document.querySelector(".themeToggler");
+
+menuBtn.addEventListener('click',()=>{
+    sideMenu.style.display = 'block';
+})
+closeBtn.addEventListener('click',()=>{
+    sideMenu.style.display = 'none';
+})
